644e64d5a8ae0be5db88911c9d5eef3ef649bb53
Vellutia <federickjonathan22@gmail.com>
2020-03-26 17:31:43 +0700

    change GlobalSummaryBloc to HydratedBloc

--- lib/bloc/feature/global_summary_bloc.dart
+++ lib/bloc/feature/global_summary_bloc.dart
@@ -4,6 +4,7 @@ import 'package:covid19_monitor/model/value_model.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 import '../../model/global_summary_model.dart';
 import '../../repository/global_summary_repository.dart';
@@ -11,7 +12,8 @@ import '../../repository/global_summary_repository.dart';
 part 'global_summary_event.dart';
 part 'global_summary_state.dart';
 
-class GlobalSummaryBloc extends Bloc<GlobalSummaryEvent, GlobalSummaryState> {
+class GlobalSummaryBloc
+    extends HydratedBloc<GlobalSummaryEvent, GlobalSummaryState> {
   final GlobalSummaryRepository globalSummaryRepository;
 
   GlobalSummaryBloc({
@@ -19,7 +21,8 @@ class GlobalSummaryBloc extends Bloc<GlobalSummaryEvent, GlobalSummaryState> {
   });
 
   @override
-  GlobalSummaryState get initialState => GlobalSummaryInitial();
+  GlobalSummaryState get initialState =>
+      super.initialState ?? GlobalSummaryInitial();
 
   @override
   Stream<GlobalSummaryState> mapEventToState(
@@ -36,6 +39,34 @@ class GlobalSummaryBloc extends Bloc<GlobalSummaryEvent, GlobalSummaryState> {
         event.time,
         summary,
       );
+    }
+  }
+
+  @override
+  GlobalSummaryState fromJson(Map<String, dynamic> json) {
+    final time = json['time'] as String;
+    final summary = GlobalSummary.fromJson(json['summary']);
+    try {
+      return GlobalSummaryLoaded(
+        time,
+        summary,
+      );
+    } catch (_) {
+      return null;
+    }
+  }
+
+  @override
+  Map<String, dynamic> toJson(GlobalSummaryState state) {
+    final time = (state as GlobalSummaryLoaded).time;
+    final summary = (state as GlobalSummaryLoaded).summary.toJson();
+    try {
+      return {
+        'time': time,
+        'summary': summary,
+      };
+    } catch (_) {
+      return null;
     }
   }
 }
